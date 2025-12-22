3d94452f91c6006ae93a28d5bd15c5b03ff07f10
Kuravi H <kuravih@gmail.com>
2025-12-22 12:42:54 -0500

    Separate tests in groups

--- tests/test_pyshmio_create.py
+++ tests/test_pyshmio_create.py
@@ -1,28 +1,26 @@
 import unittest
-from pyshmio import SharedMemory
+from pyshmio import SharedMemory, Keyword, KeywordType, DataType
 from pykato.log import setup_logger
 
-logger = setup_logger("pyshmio_open", terminator="\n")
+logger = setup_logger("pyshmio_create", terminator="\n")
 
 
-class TestPyshmioOpen(unittest.TestCase):
+class TestPyshmioCreate(unittest.TestCase):
     # pylint: disable=missing-class-docstring
 
-    def test_SharedMemory_stbsink_open(self):
-        shm = SharedMemory("stb001_stbsink")
+    def test_SharedMemory_create(self):
+        shm = SharedMemory.create("source_8uint", 100 * 100, DataType.UINT8)
         assert isinstance(shm, SharedMemory)
         logger.info("shm.creation_time             : %s", shm.creation_time)
         logger.info("shm.last_access_time          : %s", shm.last_access_time)
         logger.info("shm.size                      : %s", shm.size)
         logger.info("shm.name                      : %s", shm.name)
-        for ikey, key in enumerate(shm.keywords):
-            logger.info("keyword %d = keywords[%s] = {.value = %s,.type = %s,.comment = %s}", ikey, key, shm.keywords[key].value, shm.keywords[key].type, shm.keywords[key].comment)
-        logger.info("shm.shape                     : %s", shm.ndarray.shape)
-        logger.info("shm.dtype                     : %s", shm.ndarray.dtype)
 
-    def test_SharedMemory_stbsource_open(self):
-        shm = SharedMemory("stb001_stbsource")
-        assert isinstance(shm, SharedMemory)
+    def test_SharedMemory_create_with_keywords(self):
+        width, height = 10, 10
+        kw_width = Keyword("WIDTH", KeywordType.LONG, width, "Width")
+        kw_height = Keyword("HEIGHT", KeywordType.LONG, height, "Height")
+        shm = SharedMemory.create("source_16uint", width * height, DataType.UINT16, [kw_width, kw_height])
         logger.info("shm.creation_time             : %s", shm.creation_time)
         logger.info("shm.last_access_time          : %s", shm.last_access_time)
         logger.info("shm.size                      : %s", shm.size)
--- tests/test_pyshmio_push_pull.py
+++ tests/test_pyshmio_push_pull.py
@@ -3,41 +3,11 @@ import time
 from pyshmio import SharedMemory, Keyword, KeywordType, DataType
 from pykato.log import setup_logger
 
-logger = setup_logger("pyshmio", terminator="\n")
+logger = setup_logger("pyshmio_push_pull", terminator="\n")
 
 
-class TestPyshmio(unittest.TestCase):
+class TestPyshmioPushPull(unittest.TestCase):
     # pylint: disable=missing-class-docstring
-
-    def test_SharedMemory_create(self):
-        shm = SharedMemory.create("source_8uint", 100 * 100, DataType.UINT8)
-        assert isinstance(shm, SharedMemory)
-        logger.info("shm.creation_time             : %s", shm.creation_time)
-        logger.info("shm.last_access_time          : %s", shm.last_access_time)
-        logger.info("shm.size                      : %s", shm.size)
-        logger.info("shm.name                      : %s", shm.name)
-
-    def test_SharedMemory_create_with_keywords(self):
-        width, height = 10, 10
-        kw_width = Keyword("WIDTH", KeywordType.LONG, width, "Width")
-        kw_height = Keyword("HEIGHT", KeywordType.LONG, height, "Height")
-        shm = SharedMemory.create("source_16uint", width * height, DataType.UINT16, [kw_width, kw_height])
-        logger.info("shm.creation_time             : %s", shm.creation_time)
-        logger.info("shm.last_access_time          : %s", shm.last_access_time)
-        logger.info("shm.size                      : %s", shm.size)
-        logger.info("shm.name                      : %s", shm.name)
-        for ikey, key in enumerate(shm.keywords):
-            logger.info("keyword %d = keywords[%s] = {.value = %s,.type = %s,.comment = %s}", ikey, key, shm.keywords[key].value, shm.keywords[key].type, shm.keywords[key].comment)
-        logger.info("shm.shape                     : %s", shm.ndarray.shape)
-        logger.info("shm.dtype                     : %s", shm.ndarray.dtype)
-
-    def test_SharedMemory_open(self):
-        shm = SharedMemory("source_8uint")
-        assert isinstance(shm, SharedMemory)
-        logger.info("shm.creation_time             : %s", shm.creation_time)
-        logger.info("shm.last_access_time          : %s", shm.last_access_time)
-        logger.info("shm.size                      : %s", shm.size)
-        logger.info("shm.name                      : %s", shm.name)
 
     def test_SharedMemory_pull_data_from_storage(self):
         # producer should be running (see /shmio_examples/producer_consumer/src/producer/producer)
